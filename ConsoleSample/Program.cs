bf31ae8f3fcc60b86f3e3bb19f0f42060ebf558d
omid <ahmadpooromid@gmail.com>
2021-09-19 13:54:55 +0430

    sample for using jsontoyaml nuget package.

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using JsonToYaml;
+using System;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = @"{
+                'Name':'Omid',
+                'Age':29,
+                'Project':{
+                        'ProjectName':'JsonToYaml Nuget Package',
+                        'ProjectDescription':'This a library for converting json to yaml in .NET',
+                        },
+                'Languages':['C#','Binary']
+                }";
+
+            var yaml = JsonToYamlConvert.ConvertJsonToYaml(json);
+
+            Console.WriteLine(yaml);
+            Console.ReadLine();
+        }
+    }
+}
